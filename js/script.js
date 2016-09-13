5f9e3793daa62fc57a3f8dcfc744c839f5e9e4d3
acoleman <acoleman13@unl.edu>
2016-09-13 15:43:04 -0500

    fixes

--- js/script.js
+++ js/script.js
@@ -3,165 +3,165 @@
 
 var $ = jQuery.noConflict();
 
-$(document).ready(function($) {
-	"use strict";
+$(document).ready(function ($) {
+    "use strict";
 
-	/* global google: false */
-	/*jshint -W018 */
+    /* global google: false */
+    /*jshint -W018 */
 
+    /*-------------------------------------------------*/
+    /* =  portfolio isotope
 	/*-------------------------------------------------*/
-	/* =  portfolio isotope
+
+    var winDow = $(window);
+    // Needed variables
+    var $container = $('.iso-call');
+    var $filter = $('.filter');
+
+    try {
+        $container.imagesLoaded(function () {
+            $container.trigger('resize');
+            $container.isotope({
+                filter: '*',
+                layoutMode: 'masonry',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear'
+                }
+            });
+        });
+    } catch (err) {
+    }
+
+    winDow.bind('resize', function () {
+        var selector = $filter.find('a.active').attr('data-filter');
+
+        try {
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
+        } catch (err) {
+        }
+        return false;
+    });
+
+    // Isotope Filter 
+    $filter.find('a').click(function () {
+        var selector = $(this).attr('data-filter');
+
+        try {
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
+        } catch (err) {
+
+        }
+        return false;
+    });
+
+
+    var filterItemA = $('.filter li a');
+
+    filterItemA.on('click', function () {
+        var $this = $(this);
+        if (!$this.hasClass('active')) {
+            filterItemA.removeClass('active');
+            $this.addClass('active');
+        }
+    });
+
+    /*-------------------------------------------------*/
+    /* =  browser detect
+	/*-------------------------------------------------*/
+    try {
+        $.browserSelector();
+        // Adds window smooth scroll on chrome.
+        if ($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch (err) {
+
+    }
+
+    /*-------------------------------------------------*/
+    /* =  Search animation
 	/*-------------------------------------------------*/
 
-	var winDow = $(window);
-		// Needed variables
-		var $container=$('.iso-call');
-		var $filter=$('.filter');
-
-		try{
-			$container.imagesLoaded( function(){
-				$container.trigger('resize');
-				$container.isotope({
-					filter:'*',
-					layoutMode:'masonry',
-					animationOptions:{
-						duration:750,
-						easing:'linear'
-					}
-				});
-			});
-		} catch(err) {
-		}
-
-		winDow.bind('resize', function(){
-			var selector = $filter.find('a.active').attr('data-filter');
-
-			try {
-				$container.isotope({ 
-					filter	: selector,
-					animationOptions: {
-						duration: 750,
-						easing	: 'linear',
-						queue	: false,
-					}
-				});
-			} catch(err) {
-			}
-			return false;
-		});
-		
-		// Isotope Filter 
-		$filter.find('a').click(function(){
-			var selector = $(this).attr('data-filter');
-
-			try {
-				$container.isotope({ 
-					filter	: selector,
-					animationOptions: {
-						duration: 750,
-						easing	: 'linear',
-						queue	: false,
-					}
-				});
-			} catch(err) {
-
-			}
-			return false;
-		});
-
-
-	var filterItemA	= $('.filter li a');
-
-		filterItemA.on('click', function(){
-			var $this = $(this);
-			if ( !$this.hasClass('active')) {
-				filterItemA.removeClass('active');
-				$this.addClass('active');
-			}
-		});
-
-	/*-------------------------------------------------*/
-	/* =  browser detect
-	/*-------------------------------------------------*/
-	try {
-		$.browserSelector();
-		// Adds window smooth scroll on chrome.
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
-		}
-	} catch(err) {
-
-	}
-
-	/*-------------------------------------------------*/
-	/* =  Search animation
-	/*-------------------------------------------------*/
-	
-	var searchToggle = $('.open-search'),
+    var searchToggle = $('.open-search'),
 		inputAnime = $(".form-search"),
 		body = $('body');
 
-	searchToggle.on('click', function(event){
-		event.preventDefault();
+    searchToggle.on('click', function (event) {
+        event.preventDefault();
 
-		if ( !inputAnime.hasClass('active') ) {
-			inputAnime.addClass('active');
-		} else {
-			inputAnime.removeClass('active');			
-		}
-	});
+        if (!inputAnime.hasClass('active')) {
+            inputAnime.addClass('active');
+        } else {
+            inputAnime.removeClass('active');
+        }
+    });
 
-	body.on('click', function(){
-		inputAnime.removeClass('active');
-	});
+    body.on('click', function () {
+        inputAnime.removeClass('active');
+    });
 
-	var elemBinds = $('.open-search, .form-search');
-	elemBinds.bind('click', function(e) {
-		e.stopPropagation();
-	});
+    var elemBinds = $('.open-search, .form-search');
+    elemBinds.bind('click', function (e) {
+        e.stopPropagation();
+    });
 
+    /* ---------------------------------------------------------------------- */
+    /*	Accordion
 	/* ---------------------------------------------------------------------- */
-	/*	Accordion
-	/* ---------------------------------------------------------------------- */
-	var clickElem = $('a.accord-link');
+    var clickElem = $('a.accord-link');
 
-	clickElem.on('click', function(e){
-		e.preventDefault();
+    clickElem.on('click', function (e) {
+        e.preventDefault();
 
-		var $this = $(this),
+        var $this = $(this),
 			parentCheck = $this.parents('.accord-elem'),
 			accordItems = $('.accord-elem'),
 			accordContent = $('.accord-content');
-			
-		if( !parentCheck.hasClass('active')) {
 
-			accordContent.slideUp(400, function(){
-				accordItems.removeClass('active');
-			});
-			parentCheck.find('.accord-content').slideDown(400, function(){
-				parentCheck.addClass('active');
-			});
+        if (!parentCheck.hasClass('active')) {
 
-		} else {
+            accordContent.slideUp(400, function () {
+                accordItems.removeClass('active');
+            });
+            parentCheck.find('.accord-content').slideDown(400, function () {
+                parentCheck.addClass('active');
+            });
 
-			accordContent.slideUp(400, function(){
-				accordItems.removeClass('active');
-			});
+        } else {
 
-		}
-	});
+            accordContent.slideUp(400, function () {
+                accordItems.removeClass('active');
+            });
 
+        }
+    });
+
+    /*-------------------------------------------------*/
+    /* =  Animated content
 	/*-------------------------------------------------*/
-	/* =  Animated content
-	/*-------------------------------------------------*/
 
-	try {
-		/* ================ ANIMATED CONTENT ================ */
+    try {
+        /* ================ ANIMATED CONTENT ================ */
         if ($(".animated")[0]) {
             $('.animated').css('opacity', '0');
         }
 
-        $('.triggerAnimation').waypoint(function() {
+        $('.triggerAnimation').waypoint(function () {
             var animation = $(this).attr('data-animate');
             $(this).css('opacity', '');
             $(this).addClass("animated " + animation);
@@ -172,136 +172,136 @@ $(document).ready(function($) {
                     triggerOnce: true
                 }
         );
-	} catch(err) {
+    } catch (err) {
 
-	}
+    }
 
+    /*-------------------------------------------------*/
+    /* =  remove animation in mobile device
 	/*-------------------------------------------------*/
-	/* =  remove animation in mobile device
-	/*-------------------------------------------------*/
-	if ( winDow.width() < 992 ) {
-		$('div.triggerAnimation').removeClass('animated');
-		$('div.triggerAnimation').removeClass('triggerAnimation');
-	}
+    if (winDow.width() < 992) {
+        $('div.triggerAnimation').removeClass('animated');
+        $('div.triggerAnimation').removeClass('triggerAnimation');
+    }
 
-	try {
+    try {
 
-		var SliderPost = $('.flexslider');
+        var SliderPost = $('.flexslider');
 
-		SliderPost.flexslider({
-			slideshowSpeed: 3000,
-			easing: "swing"
-		});
-	} catch(err) {
+        SliderPost.flexslider({
+            slideshowSpeed: 3000,
+            easing: "swing"
+        });
+    } catch (err) {
 
-	}
-	
-	/*-------------------------------------------------*/
-	/* = slider Testimonial
+    }
+
+    /*-------------------------------------------------*/
+    /* = slider Testimonial
 	/*-------------------------------------------------*/
 
-	var slidertestimonial = $('.bxslider');
-	try{		
-		slidertestimonial.bxSlider({
-			mode: 'vertical'
-		});
-	} catch(err) {
-	}
+    var slidertestimonial = $('.bxslider');
+    try {
+        slidertestimonial.bxSlider({
+            mode: 'vertical'
+        });
+    } catch (err) {
+    }
 
+    /* ---------------------------------------------------------------------- */
+    /*	Contact Form
 	/* ---------------------------------------------------------------------- */
-	/*	Contact Form
-	/* ---------------------------------------------------------------------- */
 
-	var submitContact = $('#submit_contact'),
+    var submitContact = $('#submit_contact'),
 		message = $('#msg');
 
-	submitContact.on('click', function(e){
-		e.preventDefault();
+    submitContact.on('click', function (e) {
+        e.preventDefault();
 
-		var $this = $(this);
-		
-		$.ajax({
-			type: "POST",
-			url: 'contact.php',
-			dataType: 'json',
-			cache: false,
-			data: $('#contact-form').serialize(),
-			success: function(data) {
+        var $this = $(this);
 
-				if(data.info !== 'error'){
-					$this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
-					message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				} else {
-					message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				}
-			}
-		});
-	});
+        $.ajax({
+            type: "POST",
+            url: 'contact.php',
+            dataType: 'json',
+            cache: false,
+            data: $('#contact-form').serialize(),
+            success: function (data) {
 
+                if (data.info !== 'error') {
+                    $this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
+                    message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+                } else {
+                    message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+                }
+            }
+        });
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /*	Header animate after scroll
 	/* ---------------------------------------------------------------------- */
-	/*	Header animate after scroll
-	/* ---------------------------------------------------------------------- */
 
-	(function() {
+    (function () {
 
-		var docElem = document.documentElement,
+        var docElem = document.documentElement,
 			didScroll = false,
 			changeHeaderOn = 50;
-			document.querySelector( 'header' );
-		function init() {
-			window.addEventListener( 'scroll', function() {
-				if( !didScroll ) {
-					didScroll = true;
-					setTimeout( scrollPage, 100 );
-				}
-			}, false );
-		}
-		
-		function scrollPage() {
-			var sy = scrollY();
-			if ( sy >= changeHeaderOn ) {
-				$( 'header' ).addClass('active');
-			}
-			else {
-				$( 'header' ).removeClass('active');
-			}
-			didScroll = false;
-		}
-		
-		function scrollY() {
-			return window.pageYOffset || docElem.scrollTop;
-		}
-		
-		init();
-		
-	})();
+        document.querySelector('header');
+        function init() {
+            window.addEventListener('scroll', function () {
+                if (!didScroll) {
+                    didScroll = true;
+                    setTimeout(scrollPage, 100);
+                }
+            }, false);
+        }
 
-	
-	/* ---------------------------------------------------------------------- */
-	/*	Contact Map 2
-	/* ---------------------------------------------------------------------- */
-	var contact = {"lat":"40.7632192", "lon":"-96.7000615"}; //Change a map coordinate here!
+        function scrollPage() {
+            var sy = scrollY();
+            if (sy >= changeHeaderOn) {
+                $('header').addClass('active');
+            }
+            else {
+                $('header').removeClass('active');
+            }
+            didScroll = false;
+        }
 
-	try {
-		var mapContainer = $('#map2');
-		mapContainer.gmap3({
-			action: 'addMarker',
-			marker:{
-				options:{
-					icon : new google.maps.MarkerImage('images/marker.png')
-				}
-			},
-			latLng: [contact.lat, contact.lon],
-			map:{
-				center: [contact.lat, contact.lon],
-				zoom: 16
-				},
-			},
-			{action: 'setOptions', args:[{scrollwheel:false}]}
+        function scrollY() {
+            return window.pageYOffset || docElem.scrollTop;
+        }
+
+        init();
+
+    })();
+
+
+    /* ---------------------------------------------------------------------- */
+    /*	Contact Map 2
+	/* ---------------------------------------------------------------------- */
+    var contact = { "lat": "40.7632192", "lon": "-96.7000615" }; //Change a map coordinate here!
+
+    try {
+        var mapContainer = $('#map2');
+        mapContainer.gmap3({
+            action: 'addMarker',
+            marker: {
+                options: {
+                    icon: new google.maps.MarkerImage('images/marker.png')
+                }
+            },
+            latLng: [contact.lat, contact.lon],
+            map: {
+                center: [contact.lat, contact.lon],
+                zoom: 16
+            },
+        },
+			{ action: 'setOptions', args: [{ scrollwheel: false }] }
 		);
-	} catch(err) {
+    } catch (err) {
 
-	}
+    }
 
 });
 
@@ -309,51 +309,51 @@ $(document).ready(function($) {
 /*	map street view mode function
 /* ---------------------------------------------------------------------- */
 function initialize() {
-	var bryantPark = new google.maps.LatLng(40.7632192,-96.7000615); //Change a map street view cordinate here! {"lat":"-33.880641", "lon":"151.204298"}
-	var panoramaOptions = {
-		position: bryantPark,
-		pov: {
-			heading: 165,
-			pitch: 0
-		},
-		zoom: 1
-	};
-	var myPano = new google.maps.StreetViewPanorama(
+    var bryantPark = new google.maps.LatLng(40.7632192, -96.7000615); //Change a map street view cordinate here! {"lat":"-33.880641", "lon":"151.204298"}
+    var panoramaOptions = {
+        position: bryantPark,
+        pov: {
+            heading: 165,
+            pitch: 0
+        },
+        zoom: 1
+    };
+    var myPano = new google.maps.StreetViewPanorama(
 		document.getElementById('map'),
 		panoramaOptions);
-	myPano.setVisible(true);
+    myPano.setVisible(true);
 }
 
 try {
-	google.maps.event.addDomListener(window, 'load', initialize);
-} catch(err) {
+    google.maps.event.addDomListener(window, 'load', initialize);
+} catch (err) {
 
 }
 
-	//Quote Form
-	var submitQuote = $('#submit-quote'),
-		message = $('#quote-msg');
+//Quote Form
+var submitQuote = $('#submit-quote'),
+    message = $('#quote-msg');
 
-	submitQuote.on('click', function(e){
-		e.preventDefault();
+submitQuote.on('click', function (e) {
+    e.preventDefault();
 
-		var $this = $(this);
-		
-		$.ajax({
-			type: "POST",
-			url: 'quote.php',
-			dataType: 'json',
-			cache: false,
-			data: $('#quote-form').serialize(),
-			success: function(data) {
+    var $this = $(this);
 
-				if(data.info !== 'error'){
-					$this.parents('form').find('input[type=text],textarea').filter(':visible').val('');
-					$this.parents('form').find('select').filter(':visible');
-					message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				} else {
-					message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				}
-			}
-		});
-	});
+    $.ajax({
+        type: "POST",
+        url: 'quote.php',
+        dataType: 'json',
+        cache: false,
+        data: $('#quote-form').serialize(),
+        success: function (data) {
+
+            if (data.info !== 'error') {
+                $this.parents('form').find('input[type=text],textarea').filter(':visible').val('');
+                $this.parents('form').find('select').filter(':visible');
+                message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+            } else {
+                message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+            }
+        }
+    });
+});
